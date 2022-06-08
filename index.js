b55fcaf9fd3bc1634f5ab44afa7d8fb265f7d4f9
PhungPV <vanphung0198@gmail.com>
2022-06-08 18:19:30 +0700

    update style  webview canvas

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ import { WebView } from "react-native-webview";
 const styles = StyleSheet.create({
   webBg: {
     width: "100%",
-    backgroundColor: "#FFF",
-    flex: 1
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    opacity: 0.3,
   },
   loadingOverlayContainer: { position: "absolute", top: 0, bottom: 0, left: 0, right: 0, alignItems: "center", justifyContent: "center" },
 });
